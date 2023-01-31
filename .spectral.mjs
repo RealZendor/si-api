33a97aaa7658b3f7988ee047a33632d6d9aa8614
avs@studiointern.de <avs@studiointern.de>
2023-01-31 20:27:53 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MDg5Nw/spectral.js?branch_slug=main&token=fa610840-de9f-4ed0-a2ef-36f3018c240b";
+export default { extends: ruleset };
